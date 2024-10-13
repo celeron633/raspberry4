71fdad4cef50cc592b2e1eb5d5fc789426b7979b
celeron633 <dengxionghui@hotmail.com>
2024-10-13 20:50:29 +0800

    esp8266_server: different files for different connection && refine multiprocess handle

--- esp8266_server/main.py
+++ esp8266_server/main.py
@@ -4,15 +4,21 @@ import socket
 import os
 import sys
 import datetime
+from typing import List
 
 import multiprocessing
 
 server_listen_addr = '0.0.0.0'
 server_listen_port = 9090
 
+process_pool: List[multiprocessing.Process]  = []
+
 def handle_client(client_sock: socket.socket, addr):
     with client_sock:
-        with open('esp8266.txt', 'w+') as f_stream:
+        curr_dt = datetime.datetime.now().strftime("%Y-%m-%d_%H:%M:%S")
+        f_name = curr_dt + f"_{addr[0]}_{addr[1]}.txt"
+        print(f_name)
+        with open(f_name, 'w+') as f_stream:
             while True:
                 data = client_sock.recv(1024)
                 if not data:
@@ -35,11 +41,21 @@ if __name__ == "__main__":
         print(f"listen on {server_listen_addr}, port: {server_listen_port}")
 
         while True:
-            print("waiting for new client...");
-            client_socket, client_addr = server_socket.accept()
+            print("waiting for new client...")
+            try:
+                client_socket, client_addr = server_socket.accept()
+            except KeyboardInterrupt as e:
+                print("KeyboardInterrupt occured while accept, quit now!")
+                exit(-1)
+            finally:
+                # 关闭所有子进程
+                for item in process_pool:
+                    if item.is_alive:
+                        item.close()
             print(f"accepted client: {client_addr}")
             p = multiprocessing.Process(target=handle_client, args=(client_socket, client_addr))
             p.start()
+            process_pool.append(p)
 
             # 父进程关闭掉客户端fd
             client_socket.close()
